9ff1144ab310ece18ee46d2bbdc6d0f36e90b795
VuH0ngQuang <96252582+VuH0ngQuang@users.noreply.github.com>
2026-06-14 00:51:58 +0700

    Add: evaluate-reverse-polish-notation - submission-5

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-5.go
@@ -0,0 +1,32 @@
+func pop(stack []int) (int, []int) {
+	return stack[len(stack) - 1], stack[:len(stack)-1]
+}
+
+func evalRPN(tokens []string) int {
+	var stack = make([]int,0, len(tokens))
+	var tmp1, tmp2 int
+	for _, val := range tokens {
+		switch val {
+			case "+":
+				tmp2, stack = pop(stack)
+				tmp1, stack = pop(stack)
+				stack = append(stack,tmp1+tmp2)
+			case "-":
+				tmp2, stack = pop(stack)
+				tmp1, stack = pop(stack)
+				stack = append(stack,tmp1-tmp2)
+			case "*":
+				tmp2, stack = pop(stack)
+				tmp1, stack = pop(stack)
+				stack = append(stack,tmp1*tmp2)
+			case "/":
+				tmp2, stack = pop(stack)
+				tmp1, stack = pop(stack)
+				stack = append(stack,tmp1/tmp2)
+			default:
+				num, _ := strconv.Atoi(val)
+				stack = append(stack,num)
+		}
+	}
+	return stack[0]
+}
