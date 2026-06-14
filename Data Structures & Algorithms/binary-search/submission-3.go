99915fbd95f025a8f7488c6a753869449d49ea68
VuH0ngQuang <96252582+VuH0ngQuang@users.noreply.github.com>
2026-06-14 10:55:50 +0700

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.go
@@ -0,0 +1,15 @@
+func search(nums []int, target int) int {
+	left, mid, right := 0, 0, len(nums) - 1
+	for left <= right {
+		mid = ((right - left) / 2) + left
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid+1
+		} else {
+			right = mid-1
+		}
+	}
+
+	return -1;
+}
