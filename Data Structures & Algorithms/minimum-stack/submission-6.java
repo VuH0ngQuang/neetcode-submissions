72872a77bf2ef3bdf4d4cf22a71b81a9510a9e27
VuH0ngQuang <96252582+VuH0ngQuang@users.noreply.github.com>
2026-06-21 23:47:34 +0700

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.java
@@ -0,0 +1,28 @@
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> min;
+
+    public MinStack() {
+        stack = new Stack<>();
+        min = new Stack<>();
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+        if (min.isEmpty() || min.peek() >= val) min.push(val);
+    }
+    
+    public void pop() {
+        if (stack.peek().equals(min.peek())) min.pop();
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
