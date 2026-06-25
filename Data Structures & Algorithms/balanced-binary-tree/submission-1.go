b5002019ed0dfe7c4e744a3bebdc9a3e5c18f3ca
VuH0ngQuang <96252582+VuH0ngQuang@users.noreply.github.com>
2026-06-25 23:38:17 +0700

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.go
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func abs (n int) int{
+    if n < 0 {
+        return -n
+    }
+    return n
+}
+
+func dfs (root *TreeNode, res *bool) int {
+    if root == nil {
+        return 0;
+    }
+    left := dfs (root.Left, res)
+    right := dfs (root.Right, res)
+    if (abs(right - left) > 1) {
+        *res = false
+    }
+    return 1 + max(left, right)
+}
+
+func isBalanced(root *TreeNode) bool {
+    res := true
+
+    dfs(root, &res)
+
+    return res
+}
+
