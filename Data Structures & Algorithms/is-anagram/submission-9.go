283e10c290d45a5f0c96b9ad765af7931328899d
VuH0ngQuang <96252582+VuH0ngQuang@users.noreply.github.com>
2026-08-25 10:02:30 +0700

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.go
@@ -0,0 +1,16 @@
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+    count := make(map[rune]int, len(s))
+    for _, r := range s {
+        count[r]++
+    }
+    for _, r := range t {
+        count[r]--
+        if count[r] < 0 {
+            return false
+        }
+    }
+    return true
+}
